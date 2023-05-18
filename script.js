b487d28017d39b9af96cf639407f6510e390a777
avieltanyiiheng <avieltop2@gmail.com>
2023-05-18 14:00:54 +0800

    test

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let computerChoice;
 
 const choice = ["rock", "paper", "scissors"];
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+let playerScore = 0;
+let computerScore = 0;
+const results = document.querySelector(".results");
+const playerText = document.querySelector(".playerScore");
+const computerText = document.querySelector(".computerScore");
 
 function getComputerChoice() {
   // get random index value
@@ -11,37 +19,66 @@ function getComputerChoice() {
 
   return computerChoice;
 }
+let playerSelection = "";
 
 function playGame() {
-  const playerSelection = prompt("chooce rps");
-  const computerSelection = getComputerChoice();
-  if (playerSelection === "rock" && computerSelection == "paper") {
-    console.log(
-      `u lose as computer choice is ${computerSelection} and your choice is ${playerSelection}`
-    );
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    console.log(
-      `u win as computer choice is ${computerSelection} and your choice is ${playerSelection}`
-    );
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    console.log(
-      `u lose as computer choice is ${computerSelection} and your choice is ${playerSelection}`
-    );
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log(
-      `u win as computer choice is ${computerSelection} and your choice is ${playerSelection}`
-    );
-  } else if (playerSelection === computerSelection) {
-    console.log(`dfraw ${computerSelection}  ${playerSelection}`);
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    console.log(
-      `u lose as computer choice is ${computerSelection} and your choice is ${playerSelection}`
-    );
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    console.log(
-      `u win as computer choice is ${computerSelection} and your choice is ${playerSelection}`
-    );
+  if (playerScore !== 5 || computerScore !== 5) {
+    const computerSelection = getComputerChoice();
+    if (playerSelection === "rock" && computerSelection == "paper") {
+      results.textContent = `u lose as computer choice is ${computerSelection} and your choice is ${playerSelection}`;
+      computerScore++;
+      playerText.textContent = `PlayerScore: ${playerScore}`;
+      computerText.textContent = `ComputerScore: ${computerScore}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      results.textContent = `u win as computer choice is ${computerSelection} and your choice is ${playerSelection}`;
+      playerScore++;
+      playerText.textContent = `PlayerScore: ${playerScore}`;
+      computerText.textContent = `ComputerScore: ${computerScore}`;
+    } else if (
+      playerSelection === "paper" &&
+      computerSelection === "scissors"
+    ) {
+      results.textContent = `u lose as computer choice is ${computerSelection} and your choice is ${playerSelection}`;
+      computerScore++;
+      playerText.textContent = `PlayerScore: ${playerScore}`;
+      computerText.textContent = `ComputerScore: ${computerScore}`;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      results.textContent = `u win as computer choice is ${computerSelection} and your choice is ${playerSelection}`;
+      playerScore++;
+      playerText.textContent = `PlayerScore: ${playerScore}`;
+      computerText.textContent = `ComputerScore: ${computerScore}`;
+    } else if (playerSelection === computerSelection) {
+      results.textContent = `dfraw ${computerSelection}  ${playerSelection}`;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      results.textContent = `u lose as computer choice is ${computerSelection} and your choice is ${playerSelection}`;
+      computerScore++;
+      playerText.textContent = `PlayerScore: ${playerScore}`;
+      computerText.textContent = `ComputerScore: ${computerScore}`;
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "paper"
+    ) {
+      results.textContent = `u win as computer choice is ${computerSelection} and your choice is ${playerSelection}`;
+      playerScore++;
+      playerText.textContent = `PlayerScore: ${playerScore}`;
+      computerText.textContent = `ComputerScore: ${computerScore}`;
+    }
+  } else {
+    results.textContent = "GAMEOVER";
   }
 }
 
-playGame();
+rock.addEventListener("click", function () {
+  playerSelection = "rock";
+  playGame();
+});
+
+paper.addEventListener("click", function () {
+  playerSelection = "paper";
+  playGame();
+});
+
+scissors.addEventListener("click", function () {
+  playerSelection = "scissors";
+  playGame();
+});
